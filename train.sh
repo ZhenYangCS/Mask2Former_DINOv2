de2f545ccd5c64c29f98a0567f95f5f0ea3c93e8
ZhenYangCS <zheny.cs@gmail.com>
2023-05-12 13:51:31 +0800

    同步train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,9 @@
-python train_net.py --num-gpus 8 \
+python train_net.py --num-gpus 3 \
   --config-file configs/coco/instance-segmentation/dinov2/dinov2_vit_base_bs.yaml \
   --resume
 
 
 # 使用num-gpus控制GPU的数量
+
+# hfai python train_net.py --num-gpus 3 --config-file configs/coco/instance-segmentation/dinov2/dinov2_vit_base_bs.yaml --resume -- --nodes 3 --name test
+# hfai bash train.sh -- --nodes 3 --name test
